75e58499374d6a40dba80309c400de1659665860
Jomit <jovagh@microsoft.com>
2016-08-16 21:28:21 -0700

    updated guest exec powershell for remove and unregistering the app type
    added port number for ServiceManifest

--- DeployGuestExec.ps1
+++ DeployGuestExec.ps1
@@ -6,17 +6,20 @@ cd "C:\Program Files\Microsoft SDKs\Service Fabric\ClusterSetup\"
 Connect-ServiceFabricCluster localhost:19000
 
 Write-Host 'Copying application package...'
-Copy-ServiceFabricApplicationPackage -ApplicationPackagePath 'C:\SFTraining\Demos\GuestExec' -ImageStoreConnectionString 'file:C:\SfDevCluster\Data\ImageStoreShare' -ApplicationPackagePathInImageStore 'Store\NodeApp'
+Copy-ServiceFabricApplicationPackage -ApplicationPackagePath 'C:\github\servicefabricdemos\GuestExec' -ImageStoreConnectionString 'file:C:\SfDevCluster\Data\ImageStoreShare' -ApplicationPackagePathInImageStore 'Store\NodeApp'
 
 Write-Host 'Registering application type...'
 Register-ServiceFabricApplicationType -ApplicationPathInImageStore 'Store\NodeApp'
 
+Write-Host 'Creating new Application Instance...'
 New-ServiceFabricApplication -ApplicationName 'fabric:/NodeApp' -ApplicationTypeName 'NodeAppType' -ApplicationTypeVersion 1.0
 
+Write-Host 'Creating new Service Instance...'
 New-ServiceFabricService -ApplicationName 'fabric:/NodeApp' -ServiceName 'fabric:/NodeApp/NodeAppService' -ServiceTypeName 'NodeApp' -Stateless -PartitionSchemeSingleton -InstanceCount 1
 
 
+Write-Host 'Remove application...'
+Remove-ServiceFabricApplication 'fabric:/NodeApp'
 
-#Unpublish-ServiceFabricApplication -ApplicationName "fabric:/NodeApp"
-
-#Remove-ServiceFabricApplicationType -ApplicationTypeName NodeAppType -ApplicationTypeVersion 1.0
+Write-Host 'Unregister application type...'
+Unregister-ServiceFabricApplicationType -ApplicationTypeName 'NodeAppType' -ApplicationTypeVersion 1.0
