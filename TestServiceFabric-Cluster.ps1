6afef4b49169d8fa2dbf9daa65b830973399bee3
Jomit <jovagh@microsoft.com>
2016-08-23 09:02:30 -0700

    added chaos testing script

---
+++ TestServiceFabric-Cluster.ps1
@@ -0,0 +1,32 @@
+﻿$connection = "localhost:19000"
+$timeToRun = 2 #60
+$maxStabilizationTimeSecs = 180
+$waitTimeBetweenIterationsSec = 60
+
+Connect-ServiceFabricCluster $connection
+
+# Run the chaos test scenario
+#-----------------------------------------------
+
+$concurrentFaults = 3
+
+Invoke-ServiceFabricChaosTestScenario `
+    -TimeToRunMinute $timeToRun `
+    -MaxClusterStabilizationTimeoutSec $maxStabilizationTimeSecs `
+    -MaxConcurrentFaults $concurrentFaults `
+    -EnableMoveReplicaFaults `
+    -WaitTimeBetweenIterationsSec $waitTimeBetweenIterationsSec
+
+
+# Run the failover test scenario
+#-----------------------------------------------
+
+$waitTimeBetweenFaultsSec = 10
+$serviceName = "fabric:/Voting/VotingService"
+
+Invoke-ServiceFabricFailoverTestScenario `
+    -TimeToRunMinute $timeToRun `
+    -MaxServiceStabilizationTimeoutSec $maxStabilizationTimeSecs `
+    -WaitTimeBetweenFaultsSec $waitTimeBetweenFaultsSec `
+    -ServiceName $serviceName `
+    -PartitionKindSingleton   #| out-file C:\SFHackathon\results.txt
